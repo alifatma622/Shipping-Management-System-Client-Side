426fa508977688c9458731a2a54e78b446e2d124
fatma elshihna <elshinafatma@gmail.com>
2025-06-30 03:26:32 +0300

    Search done in All Delivery Component

--- ShippingSystem/src/Components/delivery-men/all-delivery-men/all-delivery-men.component.ts
+++ ShippingSystem/src/Components/delivery-men/all-delivery-men/all-delivery-men.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { DeliveryManService } from './../../../Services/deliveryMan_Service/delivery-man.service';
 import { IReadDeliveryMan } from '../../../Models/deliveryMan_models/IDeliveryMan_model';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-all-delivery-men',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './all-delivery-men.component.html',
   styleUrls: ['./all-delivery-men.component.css'],
 
@@ -17,6 +18,7 @@ export class AllDeliveryMenComponent implements OnInit {
   isLoading = true;
   errorMsg = '';
   routes: any;
+  searchUserName: string = '';
 
   constructor(private deliveryManService: DeliveryManService, private router: Router) {}
 
@@ -48,4 +50,14 @@ export class AllDeliveryMenComponent implements OnInit {
   onAdd() {
     this.router.navigate(['/delivery-men/add']);
   }
+
+  get filteredDeliveryMen(): IReadDeliveryMan[] {
+    if (!this.searchUserName.trim()) return this.deliveryMen;
+    return this.deliveryMen.filter(man =>
+      man.userName?.toLowerCase().includes(this.searchUserName.trim().toLowerCase())
+    );
   }
+  onSearchChange(value: string) {
+    this.searchUserName = value;
+  }
+}
